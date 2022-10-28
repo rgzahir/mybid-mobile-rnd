2827a84f80debcc0df538ca2d15b666beccce2ad
rgzahir <zahir.ayob@regovtech.com>
2022-10-28 17:28:33 +0800

    icon button component

---
+++ src/components/buttons/IconButton.tsx
@@ -0,0 +1,22 @@
+import React, { Fragment, FunctionComponent } from "react";
+import { TouchableOpacity } from "react-native";
+
+import { IcoMoon } from "../../icons/CustomIcon";
+
+export const IconButton: FunctionComponent<TIconButtonProps> = ({
+  disabledPress,
+  color,
+  name,
+  onPressHandler,
+  size,
+}) => {
+  return (
+    <Fragment>
+      <TouchableOpacity onPress={onPressHandler} disabled={disabledPress}>
+        <IcoMoon name={name} size={size} color={color} />
+      </TouchableOpacity>
+    </Fragment>
+  );
+};
+
+export default IconButton;
