65e6a5766a9268a6780d71de1e910452d48c3de8
rgzahir <zahir.ayob@regovtech.com>
2022-11-01 12:30:49 +0800

    make new slider button components

---
+++ src/components/sliders/buttonSlider.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const buttonSlider = () => {
+  return (
+    <View>
+      <Text>Button Slider Component</Text>
+    </View>
+  );
+};
+
+export default buttonSlider;
