c326bb15fd5a7f87df91d4d732a347da943522e4
rgzahir <zahir.ayob@regovtech.com>
2023-10-31 14:24:22 +0800

    config for jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // preset: "ts-jest",
+  // transform: {
+  //   "^.+\\.(ts|tsx)?$": "ts-jest",
+  //   "^.+\\.(js|jsx)$": "babel-jest",
+  // },
+  preset: "react-native",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
